0ba3030864fd16f400a0faaf67c6494debcccdc9
Nabila Fitri <nabilafitri@MacBook-Pro-Nabila.local>
2026-04-27 10:13:19 +0700

    menambahkan migration loan dan item

---
+++ database/migrations/2026_04_27_025758_create_loans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('item_id');
+            $table->string('nama_peminjam');
+            $table->date('borrow_date');
+            $table->date('return_date')->nullable();
+            $table->date('due_date')->nullable();
+            $table->enum('status', ['dipinjam', 'kembali', 'hilang', 'rusak'])->default('dipinjam');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// route login
+Route::get('/login', [AuthController::class, 'showlogin']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
+
+// route halaman dashboard
+Route::get('/admin', function() {
+    $totalBarang = Item::count();
+    $dipinjam = Loan::where('status', 'dipinjam')->count();
+    $kembali = Loan::where('status', 'kembali')->count();
+
+    return view('admin', compact('total_Barang', 'dipinjam', 'kembali'));
+})->middleware('role:admin');
+
+Route::get('/petugas', function() {
+    return view('petugas');
+})->middleware('role:petugas');
+
+Route::get('/siswa', function() {
+    return view('siswa');
+})->middleware('role:siswa');
+
